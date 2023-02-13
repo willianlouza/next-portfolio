5499c3c2e578ffd562fe9b5607963b0b5ba8733b
Willian Louza <willianlouza@gmail.com>
2023-02-13 15:04:34 -0300

    change background size

--- src/components/layout/background/index.tsx
+++ src/components/layout/background/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function Background() {
   return (
     <>
-      <div className="fixed -z-10 h-full w-full opacity-10">
+      <div className="absolute -z-10 h-full w-full opacity-10">
         <Image
           className="w-full h-full object-fill"
           src="/images/noise.jpg"
@@ -12,7 +12,7 @@ export default function Background() {
         />
       </div>
       <div className="fixed -z-50 h-full w-full opacity-10">
-        <Image className="w-full h-full object-fill" src="/images/glow.jpg" alt="noise-bg" width={1920} height={1080} />
+        <Image className="w-full h-full object-cover" src="/images/glow.jpg" alt="noise-bg" width={1920} height={1080} />
       </div>
 
       <div className="absolute h-full w-full  top-0 left-0 -z-20 [filter:url(#gooey)]">
